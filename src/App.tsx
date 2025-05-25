8c251e16b27e999d494c7ec79989ba7361f15102
garyMamaniCortez <mamanijhonny2210@gmail.com>
2025-05-25 10:35:26 -0400

    se añadio la ruta para login

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import Login from "./pages/Login";
 import CrearSeccion from "./pages/CrearSeccion";
 import NotFound from "./pages/NotFound";
 import CrearExamen from "./pages/CrearExamen";
@@ -21,6 +22,7 @@ const App: React.FC = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/lecciones" element={<Lecciones />} />
           <Route path="/crear-seccion" element={<CrearSeccion />} />
           <Route path="/crear-examen" element={<CrearExamen />} />
